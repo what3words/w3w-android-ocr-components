13136a75a6a2223aa3467bc23fa8d27d10c79f6e
Emanuel Amiguinho <emanuel@what3words.com>
2023-08-24 11:09:43 +0100

    nit

--- lib/src/main/java/com/what3words/ocr/components/ui/OcrScanComposable.kt
+++ lib/src/main/java/com/what3words/ocr/components/ui/OcrScanComposable.kt
@@ -48,6 +48,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -476,7 +477,7 @@ private fun SuggestionPicker(
                     displayUnits = displayUnits,
                     textStyles = suggestionTextStyles,
                     colors = suggestionColors,
-                    isLand = item.country != "ZZ",
+                    isLand = item.country.lowercase() != "zz",
                     nearestPlacePrefix = suggestionNearestPlacePrefix,
                     onClick = {
                         onSuggestionSelected.invoke(item)
