65d82359aa8f1381e51c475e6a11ad964d9dccf0
Duy Phạm <duypham@W3WMBP16A1044.local>
2024-10-30 13:50:05 +0700

    Add `CompletableDeferred` to `W3WImageAnalyzer` for async processing completion
        - Updated `W3WImageAnalyzer` to accept a `CompletableDeferred` in `onFrameCaptured` callback.
        - Clients now use `CompletableDeferred` to signal completion of asynchronous image processing.
        - `W3WImageAnalyzer` waits for the `CompletableDeferred` to complete before releasing the `ImageProxy`, preventing premature resource release.

--- lib/src/main/java/com/what3words/ocr/components/internal/W3WImageAnalysis.kt
+++ lib/src/main/java/com/what3words/ocr/components/internal/W3WImageAnalysis.kt
@@ -6,17 +6,25 @@ import androidx.camera.core.resolutionselector.ResolutionSelector
 import androidx.compose.ui.layout.LayoutCoordinates
 import com.what3words.core.types.common.W3WError
 import com.what3words.core.types.image.W3WImage
+import kotlinx.coroutines.CompletableDeferred
 import java.util.concurrent.ExecutorService
 
 /**
  * Builds an [ImageAnalysis] instance with a [W3WImageAnalyzer] for processing images.
+ *
+ * @param imageAnalyzerExecutor An [ExecutorService] to execute the image analysis on.
+ * @param cropLayoutCoordinates The [LayoutCoordinates] of the crop area view.
+ * @param cameraLayoutCoordinates The [LayoutCoordinates] of the camera view.
+ * @param aspectRatioStrategy The [AspectRatioStrategy] to use for setting the camera resolution.
+ * @param onFrameCaptured A callback invoked when a frame is captured by the camera.
+ * @param onError A callback invoked when an error occurs.
  */
 internal fun buildW3WImageAnalysis(
     imageAnalyzerExecutor: ExecutorService,
     cropLayoutCoordinates: LayoutCoordinates,
     cameraLayoutCoordinates: LayoutCoordinates,
     aspectRatioStrategy: AspectRatioStrategy = AspectRatioStrategy.RATIO_16_9_FALLBACK_AUTO_STRATEGY,
-    onFrameCaptured: (W3WImage) -> Unit,
+    onFrameCaptured: ((W3WImage) -> CompletableDeferred<Unit>),
     onError: (W3WError) -> Unit,
 ): ImageAnalysis {
 
