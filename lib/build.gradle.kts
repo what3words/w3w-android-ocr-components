6347e6e69f56465086c484e9948362b547abc089
Duy Phạm <duypham@W3WMBP16A1044.local>
2024-11-07 15:02:33 +0700

    Setup a GMD for running instrumental test

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -76,6 +76,21 @@ android {
             withSourcesJar()
         }
     }
+
+    testOptions {
+        managedDevices {
+            localDevices {
+                create("pixel2api30") {
+                    // Use device profiles you typically see in Android Studio.
+                    device = "Pixel 2"
+                    // Use only API levels 27 and higher.
+                    apiLevel = 30
+                    // To include Google services, use "google".
+                    systemImageSource = "aosp"
+                }
+            }
+        }
+    }
 }
 
 dependencies {
