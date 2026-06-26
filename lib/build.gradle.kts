26de74cf11b93156e374311cd582de0738530efb
Duy Phạm <duypham@MacBook-Pro-9.local>
2026-06-26 23:38:51 +0700

    Fix publish failing

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,13 +1,10 @@
-import java.util.Base64
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.android.library)
-    id("maven-publish")
-    id("signing")
     alias(libs.plugins.dokka)
     alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.jreleaser)
+    alias(libs.plugins.vanniktech.maven.publish)
     id("kotlin-parcelize")
     id("jacoco")
 }
@@ -15,10 +12,11 @@ plugins {
 group = "com.what3words"
 
 /**
- * IS_SNAPSHOT_RELEASE property will be automatically added to the root gradle.properties file by the CI pipeline, depending on the GitHub branch.
- * A snapshot release is generated for every pull request merged or commit made into an epic branch.
+ * Pass `-Psnapshot` on the Gradle command line to publish a `-SNAPSHOT` build.
+ * The CI pipeline does this for pushes to the `staging` branch; pushes to `main`
+ * publish a regular release.
  */
-val isSnapshotRelease = findProperty("IS_SNAPSHOT_RELEASE") == "true"
+val isSnapshotRelease = hasProperty("snapshot")
 version =
     if (isSnapshotRelease) "${findProperty("LIBRARY_VERSION")}-SNAPSHOT" else "${findProperty("LIBRARY_VERSION")}"
 
@@ -64,11 +62,6 @@ android {
     packaging {
         resources {
             excludes += "META-INF/LICENSE*"
-        }
-    }
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
         }
     }
 
@@ -164,114 +157,35 @@ tasks.register("checkSnapshotDependencies") {
 }
 
 //region publishing
+mavenPublishing {
+    coordinates("com.what3words", "w3w-android-ocr-components", version.toString())
 
-//region publishing
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            afterEvaluate {
-                from(components["release"])
-            }
+    publishToMavenCentral()
+    signAllPublications()
 
-            groupId = "com.what3words"
-            artifactId = "w3w-android-ocr-components"
-            version = project.version.toString()
+    pom {
+        name.set("w3w-android-ocr-components")
+        description.set("Android OCR UI Components that work with Google MLKit")
+        url.set("https://github.com/what3words/w3w-android-ocr-components")
 
-            withType(MavenPublication::class.java) {
-                val publicationName = name
-                val dokkaJar =
-                    project.tasks.register("${publicationName}DokkaJar", Jar::class) {
-                        group = JavaBasePlugin.DOCUMENTATION_GROUP
-                        description = "Assembles Kotlin docs with Dokka into a Javadoc jar"
-                        archiveClassifier.set("javadoc")
-                        from(tasks.named("dokkaGeneratePublicationHtml"))
-
-                        // Each archive name should be distinct, to avoid implicit dependency issues.
-                        // We use the same format as the sources Jar tasks.
-                        // https://youtrack.jetbrains.com/issue/KT-46466
-                        archiveBaseName.set("${archiveBaseName.get()}-$publicationName")
-                    }
-                artifact(dokkaJar)
-                pom {
-                    name.set("w3w-android-ocr-components")
-                    description.set("Android OCR UI Components that work with Google MLKit")
-                    url.set("https://github.com/what3words/w3w-android-ocr-components")
-
-                    licenses {
-                        license {
-                            name.set("The MIT License (MIT)")
-                            url.set("https://github.com/what3words/w3w-android-ocr-components/blob/master/LICENSE")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("what3words")
-                            name.set("what3words")
-                            email.set("development@what3words.com")
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:git://github.com/what3words/w3w-android-ocr-components.git")
-                        developerConnection.set("scm:git:ssh://git@github.com:what3words/w3w-android-ocr-components.git")
-                        url.set("https://github.com/what3words/w3w-android-ocr-components/tree/master")
-                    }
-                }
-            }
-            // POM metadata
-        }
-    }
-
-    repositories {
-        maven {
-            name = "sonatypeSnapshots"
-            url = uri("https://central.sonatype.com/repository/maven-snapshots/")
-            credentials {
-                username = findProperty("MAVEN_CENTRAL_USERNAME") as? String
-                password = findProperty("MAVEN_CENTRAL_PASSWORD") as? String
+        licenses {
+            license {
+                name.set("The MIT License (MIT)")
+                url.set("https://github.com/what3words/w3w-android-ocr-components/blob/master/LICENSE")
             }
         }
-        maven {
-            name = "stagingLocal"
-            url = uri(layout.buildDirectory.dir("staging-deploy").get().asFile.absolutePath)
+        developers {
+            developer {
+                id.set("what3words")
+                name.set("what3words")
+                email.set("development@what3words.com")
+            }
+        }
+        scm {
+            connection.set("scm:git:git://github.com/what3words/w3w-android-ocr-components.git")
+            developerConnection.set("scm:git:ssh://git@github.com:what3words/w3w-android-ocr-components.git")
+            url.set("https://github.com/what3words/w3w-android-ocr-components/tree/master")
         }
     }
 }
-
-jreleaser {
-    release {
-        github {
-            repoOwner = "what3words"
-            overwrite = true
-        }
-    }
-
-    signing {
-        active.set(org.jreleaser.model.Active.ALWAYS)
-        armored.set(true)
-        publicKey.set(
-            findProperty("W3W_GPG_PUBLIC_KEY")?.toString()
-                ?.let { String(Base64.getDecoder().decode(it)) } ?: "")
-        secretKey.set(
-            findProperty("W3W_GPG_SECRET_KEY")?.toString()
-                ?.let { String(Base64.getDecoder().decode(it)) } ?: "")
-        passphrase.set(findProperty("W3W_GPG_PASSPHRASE")?.toString())
-    }
-    deploy {
-        maven {
-            mavenCentral {
-                create("sonatype") {
-                    active.set(org.jreleaser.model.Active.ALWAYS)
-                    url.set("https://central.sonatype.com/api/v1/publisher")
-                    stagingRepository(layout.buildDirectory.dir("staging-deploy").get().asFile.absolutePath)
-                    username.set(findProperty("MAVEN_CENTRAL_USERNAME")?.toString())
-                    password.set(findProperty("MAVEN_CENTRAL_PASSWORD")?.toString())
-                    verifyPom.set(false)
-                    setStage(org.jreleaser.model.api.deploy.maven.MavenCentralMavenDeployer.Stage.UPLOAD.toString())
-                }
-            }
-        }
-    }
-}
-//endregion
-
 //endregion
